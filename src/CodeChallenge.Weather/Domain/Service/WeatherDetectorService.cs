eeb77065fbf7bd7015fdbf7ce7a53fa987821b71
isaac <gusilight80@gmail.com>
2021-12-22 09:54:19 +0100

    refactor: delete comments

--- src/CodeChallenge.Weather/Domain/Service/WeatherDetectorService.cs
+++ src/CodeChallenge.Weather/Domain/Service/WeatherDetectorService.cs
@@ -1,14 +1,11 @@
-﻿using System;
-
-namespace CodeChallenge.Weather.Domain.Service
+﻿namespace CodeChallenge.Weather.Domain.Service
 {
+    using System;
+
     public class WeatherDetectorService
     {
         public string GetWeather(SensorsWeather sensor)
         {
-            // TODO: This is a simple example, just to pass the tests is incompleted.
-            //       Here is where the candidate should complet the implementation.
-
             if (sensor.TemperatureInCelsius == 20)
             {
                 return "is having a good weather";
